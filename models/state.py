2aa6abe1c37502097e74f0259904039133525ee7
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-03-23 03:23:40 -0700

    the models

---
+++ models/state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_t
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class State(BaseModel):
+    """ State class """
+    __tablename__ = 'states'
+    if storage_t == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """returns the list of city instances"""
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.value():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
