05a118167a1883108c9b96b8aaeeb57d5a1433f4
p-e-a-c-e2 <smartpeace6@gmail.com>
2023-04-23 14:05:12 -0700

    engine

---
+++ models/engine/file_storage.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+import json
+
+
+class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        if cls is None:
+            return self.__objects
+        dict = {}
+        for key, value in self.__objects.items():
+            if value.__class__ == cls:
+                dict[key] = value
+        return dict
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        if obj:
+            key = '{}.{}'.format(type(obj.__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """Saves storage dictionary to file"""
+        my_dict = {}
+        for key, value in self.__objects.items():
+            my_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+            json.dump(my_dict, f)
+
+    def reload(self):
+        """Loads storage dictionary from file"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
+        classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
+        try:
+            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+                for key, value in (json.load(f)).items():
+                    value = eval(value["__class__"])(**value)
+                    self.__objects[key] = value
+        except FileNotFoundError:
+            pass
+
+
+    def delete(self, obj=None):
+        """deletes the obj from the attr"""
+        if obj is None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        """ call the reload method"""
+        reload()
